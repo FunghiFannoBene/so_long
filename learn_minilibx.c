73bee1d36270c9e3ac70ec44102247b31efe8e79
FunghiFannoBene <119907315+FunghiFannoBene@users.noreply.github.com>
2023-07-13 22:29:08 +0200

    Create learn_minilibx.c

---
+++ learn_minilibx.c
@@ -0,0 +1,44 @@
+#include <mlx.h>
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
+}
+
+
+//apertura di una finestra GRANDEZZA 1920 X 1080 con nome app, hello world.
+
+#include <mlx.h>
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+int	main(void)
+{
+	void	*mlx;
+	t_data	img;
+
+	mlx = mlx_init();
+	img.img = mlx_new_image(mlx, 1920, 1080);
+
+	/*
+	** After creating an image, we can call `mlx_get_data_addr`, we pass
+	** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
+	** then be set accordingly for the *current* data address.
+	*/
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+}
+
+// RECUPERO MEMORY ADDRESS
+
